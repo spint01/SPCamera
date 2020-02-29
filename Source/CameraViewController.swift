b662f32e80da6a42b905da56d30ed15fb7cabda2
Steve Pint <steve.pint@gmail.com>
2020-02-28 21:08:41 -0600

    Remove code prior to iOS 12

--- Source/CameraViewController.swift
+++ Source/CameraViewController.swift
@@ -367,31 +367,24 @@ open class CameraViewController: UIViewController {
 			var defaultVideoDevice: AVCaptureDevice?
 
 			// Choose the back dual camera if available, otherwise default to a wide angle camera.
-            if #available(iOS 10.2, *) {
-                if let dualCameraDevice = AVCaptureDevice.default(.builtInDualCamera, for: .video, position: .back) {
-                    defaultVideoDevice = dualCameraDevice
-                } else if let backCameraDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
-                    // If the back dual camera is not available, default to the back wide angle camera.
-                    defaultVideoDevice = backCameraDevice
-                } else if let frontCameraDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front) {
-                    /*
-                     In some cases where users break their phones, the back wide angle camera is not available.
-                     In this case, we should default to the front wide angle camera.
-                     */
-                    defaultVideoDevice = frontCameraDevice
-                }
-            } else {
-                if let backCameraDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
-                    // If the back dual camera is not available, default to the back wide angle camera.
-                    defaultVideoDevice = backCameraDevice
-                } else if let frontCameraDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front) {
-                    /*
-                     In some cases where users break their phones, the back wide angle camera is not available.
-                     In this case, we should default to the front wide angle camera.
-                     */
-                    defaultVideoDevice = frontCameraDevice
-                }
+
+            if let dualCameraDevice = AVCaptureDevice.default(.builtInDualCamera, for: .video, position: .back) {
+                defaultVideoDevice = dualCameraDevice
+                // This is iOS 13 only and isn't the camera we want for photos
+//                } else if let backCameraDevice = AVCaptureDevice.default(.builtInDualWideCamera, for: .video, position: .back) {
+//                    // If the back dual camera is not available, default to the back wide angle camera.
+//                    defaultVideoDevice = backCameraDevice
+            } else if let backCameraDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
+                // If the back dual camera is not available, default to the back wide angle camera.
+                defaultVideoDevice = backCameraDevice
+            } else if let frontCameraDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front) {
+                /*
+                 In some cases where users break their phones, the back wide angle camera is not available.
+                 In this case, we should default to the front wide angle camera.
+                 */
+                defaultVideoDevice = frontCameraDevice
             }
+
             if defaultVideoDevice == nil {
                 print("No AVCaptureDevice")
                 return
